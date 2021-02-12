d04257d8edbbd6e3574f7147be28efc3cf890efc
Lalatendu Bishoyi <55109590+Lala56ai@users.noreply.github.com>
2021-02-12 16:53:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,27 +12,69 @@ import numpy as np
 import pandas as pd
 #from keras.models import model_from_json
 import json
+import pickle
+from xgboost import XGBClassifier
+import joblib
 '''
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 '''
+dict_path='./dictiornay_data'
+model_path='./'
 
-#with open('/var/www/flask_predict_api/rf.pkl', 'rb') as model_file:
- #   model = pickle.load(model_file)
+with open('{}accident_model.pkl'.format(model_path), 'rb') as model_file:
+    acc_model = pickle.load(model_file)
+
+#fname='{}accident_model.sav'.format(model_path)
+#acc_model = XGBClassifier.load_model('accident_model.json')
+
+#xgb = joblib.load(fname)
+    
+with open('{}poteltial_accident_model.pkl'.format(model_path), 'rb') as model_file:
+    pacc_model = pickle.load(model_file)
+    
+
+
+
+
+
+   
+#with open('{}\Accident_Level.json'.format(dict_path)) as json_file: 
+#    data = json.load(json_file) 
+
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
-Week_day1 = {"Sunday":0,"Monday":1,"Tuesday":2,"Wednesday":3,"Thrusday":4,"Friday":5,"Saturday":6}
-Season1 = {"Spring":1,"Summer":2,"Autumn":3,"Winter":4}
+#Week_day1 = {"Sunday":0,"Monday":1,"Tuesday":2,"Wednesday":3,"Thrusday":4,"Friday":5,"Saturday":6}
+#Season1 = {"Spring":1,"Summer":2,"Autumn":3,"Winter":4}
 
-def input_datafarame(lst):
-    df = pd.DataFrame(lst,columns=['Country','Local','Industry Sector','Gender','Employee type','Critical Risk','Year','Month','Day','Weekday','Season','Description'])
-    X_cat=df[['Country','Local','Industry Sector','Gender','Employee type','Critical Risk','Year','Month','Day','Weekday','Season']]
-    X_nlp=df[['Description']]
+def read_dictionary_data(file_name):
+    with open('{}/{}'.format(dict_path,file_name)) as json_file: 
+            data = json.load(json_file) 
+    return data
+
+def input_dataframe(lst):
+    np_array = np.array(lst)
+
+    reshaped_array = np.reshape(np_array, (1, len(lst)))
+    df = pd.DataFrame(reshaped_array,columns=['Country','Local','Industry Sector','Gender','Employee type','Critical Risk','Year','Month','Day','Weekday','Season'])
+    #X_cat=df[['Country','Local','Industry Sector','Gender','Employee type','Critical Risk','Year','Month','Day','Weekday','Season']]
+    # X_nlp=df[['Description']]
+    #print(df)
+    return df
+
+def pot_input_datafarame(lst):
+    np_array = np.array(lst)
+
+    reshaped_array = np.reshape(np_array, (1, len(lst)))
+    
+    df = pd.DataFrame(reshaped_array,columns=['Country','Local','Industry Sector','Gender','Employee type','Critical Risk','Year','Month','Day','Weekday','Season','Accident Level'])
+    #X_cat=df[['Country','Local','Industry Sector','Gender','Employee type','Critical Risk','Year','Month','Day','Weekday','Season']]
+   # X_nlp=df[['Description']]
     print(df)
-    return X_nlp,X_cat
+    return df
 '''
 def read_model():
     # load json and create model
@@ -65,7 +107,7 @@ def month2seasons(x):
     elif x in [6, 7, 8]:
         season = 'Winter'
     print("season in function:",season)
-    return Season1[season]
+    return season_lvl[season]
 
 def data_extact(x):
     print("Date Extract Started")
@@ -77,12 +119,32 @@ def data_extact(x):
     month = date.month
     print("month:",month)
     day = date.day
-    week_day = Week_day1[date.day_name()]
+    week_day = weekday_lvl[date.day_name()]
     print("week_day:",week_day)
     season = month2seasons(month)
     print("season:",season)
     print(year,month,day,week_day,season)
     return year,month,day,week_day,season
+
+def get_key(my_dict,val):
+    for key, value in my_dict.items():
+         if val == value:
+             return key
+ 
+    return "No Result"
+
+
+accident_lvl = read_dictionary_data('Accident_Level.json')    
+country_lvl = read_dictionary_data('country.json') 
+critical_Risk_lvl = read_dictionary_data('Critical_Risk.json')
+employee_type_lvl = read_dictionary_data('Employee_type.json')
+gender_lvl = read_dictionary_data('Gender.json')
+industry_sector_lvl = read_dictionary_data('Industry_Sector.json')
+local_lvl = read_dictionary_data('Local.json')
+industry_sector_lvl = read_dictionary_data('Industry_Sector.json')
+potential_Accident_Level_lvl = read_dictionary_data('Potential_Accident_Level.json')
+season_lvl = read_dictionary_data('Season.json')
+weekday_lvl = read_dictionary_data('Weekday.json')
 
 
 @app.route('/')
@@ -92,7 +154,6 @@ def home():
 @app.route('/predict', methods=["POST"])
 def predict_json():
     input= request.get_json()
-    print("Lala")
     print(input)
     #input_data = json.loads(input)
     input_data=input
@@ -101,13 +162,13 @@ def predict_json():
     
     
     data = input_data["accident_deatils"]
-    country	= data.get("Country")
+    country	= country_lvl[data.get("Country")]
     print("Country:",country)
-    local   = data.get("Local")
-    industry_Sector = data.get("Industry_Sector")
-    gender = data.get("Gender")
-    employee_type = data.get("Employee_Type")
-    critical_risk = data.get("Critical_Risk")
+    local   = local_lvl[data.get("Local")]
+    industry_Sector = industry_sector_lvl[data.get("Industry_Sector")]
+    gender = gender_lvl[data.get("Gender")]
+    employee_type = employee_type_lvl[data.get("Employee_Type")]
+    critical_risk = critical_Risk_lvl[data.get("Critical_Risk")]
     year,month,day,weekday,season=data_extact(data.get("Date"))	
     description = data.get("Description")
     
@@ -116,15 +177,33 @@ def predict_json():
 
     print(description)
 
-    # cat_data=[country,local,industry_Sector,gender,employee_type,critical_risk,year,month,day,weekday,season]
+    cat_data=[country,local,industry_Sector,gender,employee_type,critical_risk,year,month,day,weekday,season]
+    print("\n\n\n")
+    print(cat_data)
     #nlp_data,cat_data=input_datafarame([country,local,industry_Sector,gender,employee_type,critical_risk,year,month,day,weekday,season,description])
     
     #nlp_data = tokenizer_text(nlp_data)
     #print(nlp_data)
+    inut_data=input_dataframe(cat_data)
     
+    print(inut_data)
     
+    acc_pred = acc_model.predict(inut_data)
+    print(acc_pred)
+    
+    pot_inut_data=pot_input_datafarame([country,local,industry_Sector,gender,employee_type,critical_risk,year,month,day,weekday,season,acc_pred])
+    print(pot_inut_data)
+    
+    pot_acc_pred= pacc_model.predict(pot_inut_data)
+    
+    acc_lvl= get_key(accident_lvl,acc_pred)
+    pot_acc_lvl= get_key(potential_Accident_Level_lvl,pot_acc_pred)
     #model = read_model()
     #print(model.summary())
+    
+    
+    print(acc_lvl,pot_acc_lvl)
+    
     
    # y_pred = model.predict(x=[nlp_data, cat_data], batch_size=1024, verbose=1)
     y_pred=1
@@ -133,7 +212,11 @@ def predict_json():
     #print(jsonify(input))
     #input_data = pd.read_csv(request.files.get("input_file"), header=None)
     #prediction = model.predict(input_data)
+<<<<<<< HEAD
+    return jsonify({"Accident pedict":acc_lvl,"Potential Accident pedict":pot_acc_lvl})
+=======
     return jsonify({"Accident pedict":y_pred,"Potential Accident pedict":y_pred})
+>>>>>>> cf73d9bee112b682cf52a78045f4ffb578624bdd
     #return y_pred
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=5000)
